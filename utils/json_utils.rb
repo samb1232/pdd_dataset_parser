b0f276d5de7cc8c9d8128d7a947d129313a75c36
samb1232 <arterm212@gmail.com>
2025-01-08 20:46:12 +0500

    fix/added directory creation

--- utils/json_utils.rb
+++ utils/json_utils.rb
@@ -1,11 +1,23 @@
 require "json"
+require 'fileutils'
+
 
 def read_json_string(json_str)
   return JSON.parse(json_str)
 end
 
 def write_json_array_to_file(json_array, file_path)
+  ensure_directories_exist(file_path)
   File.open(file_path, 'w') do |file|
     file.write(JSON.pretty_generate(json_array))
+  end
+end
+
+
+def ensure_directories_exist(file_path)
+  directory_path = File.dirname(file_path)
+
+  unless File.directory?(directory_path)
+    FileUtils.mkdir_p(directory_path)
   end
 end
