c3b3f69bdc147e52e548788e0ce4d3c33790a8eb
samb1232 <arterm212@gmail.com>
2025-02-23 22:52:23 +0500

    promoted prompt

--- parsers/prompt_maker_for_survey.rb
+++ parsers/prompt_maker_for_survey.rb
@@ -1,5 +1,5 @@
 require_relative "../utils/json_utils"
-
+require_relative "../utils/prompt_utils"
 
 def main() 
     data = json_string_to_hash_arr(File.read("results\\data_simple_chronographic.json"))
@@ -12,24 +12,40 @@ end
 def make_prompts(puzzles_collections)
     prompts = []
     puzzles_collections.each do |puzzle_collection|
-        prompt = "INTRODUCTION"
-        prompt += "EXPLAINING CONCEPT OF PUZZLE"
+        puzzles_len = puzzle_collection["puzzles_len"]
+        next if puzzles_len > 6
+
+        project_name = get_project_name_from_link(puzzle_collection["project_name"])
+        
+        prompt = "I am working on a GitHub project titled \"#{project_name}\". "
+        prompt += "I use metodology of creating issues as \"puzzles\". When a programmer discovers error or possible improvement, he writes a comment in code, that is determened as \"puzzle\". "
+        prompt += "Then special program automaticly collects this puzzles in code and creates Github issue for each puzzle."
+        prompt += "\n\n"
+        prompt += "Currently, there are #{puzzles_len} puzzles. "
         prompt += "I will provide you with information about current active puzzles in my project in chronological order (from oldest to newest): "
 
-        puzzle_collection["puzzles"].each do |puzzle|
-            prompt += create_prompt_from_puzzle(puzzle)
-        end
+        prompt += create_prompt_from_puzzles(puzzle_collection["puzzles"])
         
+        prompt += "I would like you to decide what puzzle should i solve first. "
+        prompt += "Consider the following criteria: "
+
+        prompt += "Do not explain yourself, write only id of chosen puzzle and nothing more."
         
-
-
-        prompt += "EXPLAINING PRIORITY CRITERIA"
-        prompt += "Help me decide what puzzle should i solve first."
         prompts << prompt
     end
 
     return prompts
 end
 
+
+def create_prompt_from_puzzles(puzzles)
+    prompt = ""
+
+    puzzles.each do |puzzle|
+        prompt += "PUZZLE. "
+    end
+
+    return prompt
+end
 
 main
